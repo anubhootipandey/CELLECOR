b34cf77a848ec1c4d29edca3ef6c1eb19e36075f
saniya0729 <saniyakhan0729@gmail.com>
2024-09-19 21:43:32 -0700

    Footer Updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,119 @@
-import React from "react";
 import { BiLogoFacebook } from "react-icons/bi";
 import { AiFillYoutube } from "react-icons/ai";
 import { BiLogoInstagramAlt } from "react-icons/bi";
 import { GrTwitter } from "react-icons/gr";
+import { useState } from "react";
+import { FiMinus } from "react-icons/fi";
+import { FaPlus } from "react-icons/fa6";
 import "./Footer.css";
 
 function Footer() {
+  const [openSection, setOpenSection] = useState(null);
+
+  const toggleSection = (section) => {
+    setOpenSection(openSection === section ? null : section);
+  };
   return (
     <footer>
+      <div className="collapsible-sections md:hidden block">
+        <div className="section">
+          <h2 onClick={() => toggleSection("shop")}>
+            SHOP {openSection === "shop" ? <FiMinus /> : <FaPlus />}
+          </h2>
+          {openSection === "shop" && (
+            <div className="content">
+              <ul>
+                <li>
+                  <a href="#">TWS</a>
+                </li>
+                <li>
+                  <a href="#">Smart Watches</a>
+                </li>
+                <li>
+                  <a href="#">Neckband</a>
+                </li>
+                <li>
+                  <a href="#">Headphones</a>
+                </li>
+                <li>
+                  <a href="#">Smart TV</a>
+                </li>
+                <li>
+                  <a href="#">Mobile</a>
+                </li>
+                <li>
+                  <a href="#">Mobile Accessories</a>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+
+        <div className="section">
+          <h2 onClick={() => toggleSection("information")}>
+            INFORMATION{" "}
+            {openSection === "information" ? <FiMinus /> : <FaPlus />}
+          </h2>
+          {openSection === "information" && (
+            <div className="content">
+              <ul>
+                <li>
+                  <a href="#">About Us</a>
+                </li>
+                <li>
+                  <a href="#">Warranty Registration</a>
+                </li>
+                <li>
+                  <a href="#">Management</a>
+                </li>
+                <li>
+                  <a href="#">Blog</a>
+                </li>
+                <li>
+                  <a href="#">Careers</a>
+                </li>
+                <li>
+                  <a href="#">Track Order</a>
+                </li>
+                <li>
+                  <a href="#">Contact Us</a>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+
+        <div className="section">
+          <h2 onClick={() => toggleSection("customerService")}>
+            CUSTOMER SERVICE{" "}
+            {openSection === "customerService" ? <FiMinus /> : <FaPlus />}
+          </h2>
+          {openSection === "customerService" && (
+            <div className="content">
+              <ul>
+                <li>
+                  <a href="#">Service Center</a>
+                </li>
+                <li>
+                  <a href="#">Customer Support</a>
+                </li>
+                <li>
+                  <a href="#">Privacy Policy</a>
+                </li>
+                <li>
+                  <a href="#">Return Policy</a>
+                </li>
+                <li>
+                  <a href="#">Terms & Conditions</a>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+
       <div className="container">
-        <div className="footer-section">
+        <div className="footer-section md:block hidden ">
           <h4>Shop</h4>
           <ul>
             <li>
@@ -36,7 +140,7 @@ function Footer() {
           </ul>
         </div>
 
-        <div className="footer-section">
+        <div className="footer-section md:block hidden ">
           <h4>Information</h4>
           <ul>
             <li>
@@ -63,7 +167,7 @@ function Footer() {
           </ul>
         </div>
 
-        <div className="footer-section">
+        <div className="footer-section md:block hidden">
           <h4>Customer Service</h4>
           <ul>
             <li>
@@ -84,8 +188,8 @@ function Footer() {
           </ul>
         </div>
 
-        <div className="footer-section">
-          <h4>Newsletter Sign Up</h4>
+        <div className="footer-section ml-5">
+          <h4 className="mt-5 mb-0">Newsletter Sign Up</h4>
           <p>
             Sign up for exclusive updates, new arrivals & insider only discounts
           </p>
